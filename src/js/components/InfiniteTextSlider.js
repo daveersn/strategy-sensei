6ef05de0686a91561587ce98e2c393344fdf3755
Davide Rosina <xdavi01@gmail.com>
2024-11-06 21:58:20 +0100

    wip services sec

---
+++ src/js/components/InfiniteTextSlider.js
@@ -0,0 +1,13 @@
+export default class InfiniteTextSlider {
+  constructor(element) {
+    this.$el = element;
+    this.container = this.$el.querySelector(".slider-items");
+
+    this._init();
+  }
+
+  _init() {
+    this.container.innerHTML =
+      this.container.innerHTML + this.container.innerHTML;
+  }
+}
--- src/js/theme.js
+++ src/js/theme.js
@@ -1,4 +1,13 @@
 import UIkit from "uikit";
 import "@fontsource-variable/raleway";
+import InfiniteTextSlider from "./components/InfiniteTextSlider.js";
 
 window.uikit = UIkit;
+
+const u = UIkit.util;
+
+u.ready(() => {
+  u.$$("[infinite-text-slider]").forEach(
+    (element) => new InfiniteTextSlider(element),
+  );
+});
