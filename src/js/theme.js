eb1cc24d42a9b20468e0839abf1d1a63361675da
Davide Rosina <xdavi01@gmail.com>
2024-11-11 23:11:50 +0100

    wip

--- src/js/theme.js
+++ src/js/theme.js
@@ -2,6 +2,7 @@ import UIkit from "uikit";
 import "@fontsource-variable/raleway";
 import InfiniteTextSlider from "./components/InfiniteTextSlider.js";
 import ReviewsSlider from "./components/ReviewsSlider.js";
+import Lenis from "lenis";
 
 window.uikit = UIkit;
 
@@ -14,3 +15,53 @@ u.ready(() => {
 
   u.$$(".reviews-slider").forEach((element) => new ReviewsSlider(element));
 });
+
+u.on(window, "load", () => {
+  configureAnchorScroll();
+});
+
+// Smooth Scroll
+const lenis = new Lenis({
+  autoRaf: true,
+  duration: 1,
+});
+
+function configureAnchorScroll() {
+  let anchors = u.$$('a[href*="#"]');
+
+  const isDesktop = window.matchMedia("(min-width: 960px)");
+  let navbarPosition = null;
+
+  u.on(anchors, "click", (e) => {
+    e.preventDefault();
+
+    let attr = u.attr(e.currentTarget, "href");
+
+    let targetElement = u.$(attr);
+    let targetElementPosition = targetElement
+      ? targetElement.getBoundingClientRect()
+      : null;
+
+    let top = targetElement
+      ? targetElementPosition.top +
+        window.scrollY -
+        navbarPosition.top -
+        navbarPosition.height * 2
+      : 0;
+
+    lenis.scrollTo(targetElement, {
+      offset: -navbarPosition.top - navbarPosition.height * 2,
+    });
+  });
+
+  let updateOffset = function (isDesktop) {
+    let navbar = u.$(
+      isDesktop ? ".navbar-desktop [uk-navbar]" : ".navbar-mobile [uk-navbar]",
+    );
+
+    navbarPosition = navbar.getBoundingClientRect();
+  };
+
+  updateOffset(isDesktop.matches);
+  isDesktop.addEventListener("change", ({ matches }) => updateOffset(matches));
+}
